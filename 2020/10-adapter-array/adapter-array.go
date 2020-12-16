21807ad5cc53d7e4724b96e2f4db2e98424804dd
Scott W. Dunlop <swdunlop@gmail.com>
2020-12-15 17:26:22 -0800

    2020-10 adapter array, part 2

--- 2020/10-adapter-array/adapter-array.go
+++ 2020/10-adapter-array/adapter-array.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	solution, err := solve1(`input.txt`)
+	solution, err := solve2(`input.txt`, 168+3)
 	advent.ExitOnErr(err)
 	fmt.Println(solution)
 }
@@ -36,4 +36,44 @@ func solve1(file string) (int, error) {
 	threes++ // include the device
 
 	return ones * threes, nil
+}
+
+func solve2(file string, target int) (int, error) {
+	adapters, err := advent.ReadInts(file)
+	if err != nil {
+		return 0, err
+	}
+	sort.Ints(adapters)
+
+	t := make(table, len(adapters))
+	t[0] = entry{}
+	for _, adapter := range adapters {
+		t[adapter] = entry{}
+	}
+	return t.walk(0, target), nil
+}
+
+type table map[int]entry
+
+func (t table) walk(pos, target int) int {
+	if pos == target {
+		return 1
+	}
+
+	e, ok := t[pos]
+	if !ok {
+		return 0
+	}
+	if e.walked {
+		return e.paths
+	}
+
+	paths := t.walk(pos+1, target) + t.walk(pos+2, target) + t.walk(pos+3, target)
+	t[pos] = entry{true, paths}
+	return paths
+}
+
+type entry struct {
+	walked bool
+	paths  int
 }
--- 2020/10-adapter-array/adapter-array_test.go
+++ 2020/10-adapter-array/adapter-array_test.go
@@ -10,4 +10,12 @@ func TestSolve1(t *testing.T) {
 	test.Solve(t, solve1, `test2.txt`, 220)
 }
 
-// func TestSolve2(t *testing.T) { test.Solve(t, solve2, `test2.txt`, 126) }
+func TestSolve2(t *testing.T) {
+	test.Solve(t, func(path string) (int, error) {
+		return solve2(path, 22)
+	}, `test1.txt`, 8)
+
+	test.Solve(t, func(path string) (int, error) {
+		return solve2(path, 52)
+	}, `test2.txt`, 19208)
+}
